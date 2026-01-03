6d0242c9eb623a04d4d80e2249544c31d7f99fcf
jimmychuico <jfcpjimmy@gmail.com>
2026-01-02 22:25:31 -0500

    Add Docker Compose with SQL Server and automatic EF migrations

--- ApiPersonas/Program.cs
+++ ApiPersonas/Program.cs
@@ -5,9 +5,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("Connection");
+// cadena de conexión a la base de datos SQL Server local
+//var connectionString = builder.Configuration.GetConnectionString("Connection");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+//;
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? $"Server={Environment.GetEnvironmentVariable("DB_HOST")};" +
+       $"Database=PersonasDb;" +
+       $"User Id=sa;" +
+       $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
+       $"TrustServerCertificate=True;";
+
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString))
 
 builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
 
@@ -36,5 +48,13 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+// para ejecutar  las migraciones pendientes al iniciar la aplicación
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 app.Run();
